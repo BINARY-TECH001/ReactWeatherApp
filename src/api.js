023eb5d0f2fceff9b38e5fac8e155bc375cc3794
ABDULRAFIU MUBARAK I <rafiumubarak2019@gmail.com>
2023-02-21 00:26:59 +0100

    Pushing the weather app at First

--- src/api.js
+++ src/api.js
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
 
-export const WEATHER_API_KEY = "b3ca591389c62245e54010a72f03dc50"
+export const WEATHER_API_KEY = "b3ca591389c62245e54010a72f03dc50" 	
